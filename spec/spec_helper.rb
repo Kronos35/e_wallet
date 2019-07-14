950d343daee7f43dca0cb5cb8d9b6f28bfcaf3a5
shasticdev <dev@shastic.com>
2019-07-14 14:06:32 -0600

    Organizing webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -11,12 +8,6 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  config.before(:each) do
-    stub_request(:get, /api.github.com/).
-      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-      to_return(status: 200, body: "stubbed response", headers: {})
-  end
 
 =begin
   config.filter_run_when_matching :focus
